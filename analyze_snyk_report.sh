88339cb6c824ebafeae8e9d001c501bd42f29c4d
Raja Soundaramourty <rajasoun@icloud.com>
2021-12-21 01:58:56 +0530

    refactor code

--- analyze_snyk_report.sh
+++ analyze_snyk_report.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-cookie=$(<cookie.txt) 
-query=$(<query.txt) 
-
 function urldecode() { 
   : "${*//+/ }"; echo -e "${_//%/\\x}"; 
 }
@@ -25,26 +22,34 @@ function urlencode() {
 }
 
 function download_report(){
+  scope=$1
+  org=$2
+  url="https://app.snyk.io/org/$org/reports/issues/export"
   echo "Download Report"
-  curl -s 'https://app.snyk.io/org/cisco-lcande-pilot-ghe/reports/issues/export' \
+  cookie=$(<config/cookie-$scope.txt) 
+  query=$(<config/query-$scope.txt) 
+  curl -s  $url \
     -H 'content-type: application/x-www-form-urlencoded' \
     -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
     -H  "$cookie" \
     --data-raw "$query"\
-    --compressed > /tmp/issues.csv
+    --compressed > /tmp/issues-$scope.csv
   echo "Downloading Report Done !!!" 
 }
 
 function filter_log4j_vulnerabilities(){
   csv_file="issues.csv"
-  lg4j_cve="CVE-2021-4104|CVE-2017-5645|CVE-2020-9488|CVE-2019-17571|CVE-2021-44228|CVE-2021-45046|CVE-2021-45046"
+  log4j_cve="CVE-2021-4104|CVE-2017-5645|CVE-2020-9488|CVE-2019-17571|CVE-2021-44228|CVE-2021-45046|CVE-2021-45046"
   headers="$(cat  $csv_file | head -1)"
-  vuln_repos="$(grep -E $lg4j_cve $csv_file)"
+  vuln_repos="$(grep -E $log4j_cve $csv_file)"
   echo "$headers" > log4j_issues.csv
   echo "$vuln_repos" >> log4j_issues.csv
 }
 
-download_report
+download_report "www" "cisco-lcande-pilot-ghe"
+download_report "wwwin" "cisco-lce-pilot"
+cat /tmp/issues-www.csv <(echo) <(tail +2 /tmp/issues-wwwin.csv) > /tmp/issues.csv
+
 #filter_log4j_vulnerabilities
 python3 analyze_snyk_report.py -f /tmp/issues.csv
 cat /tmp/log4j_vulnerabilities.csv | tr -d '"' |  tr -d '[' | tr -d ']' | rev | cut -c9- | rev > log4j_issues.csv
